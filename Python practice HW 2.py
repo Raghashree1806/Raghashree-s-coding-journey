b6082e7d241d5bb5631d45957c582ab1ccc380e3
Raghashree1806 <raghashree18062008@gmail.com>
2026-09-21 21:30:49 +0530

    Add files via upload
    Ragha's first Python coding practice

---
+++ Python practice HW 2.py
@@ -0,0 +1,22 @@
+#Write Python code to print Hello World!
+print("Hello World")
+#Write Python code to print Welcome to Python
+print("Welcome to Python")
+#Write Python code to print your name
+print("Raghashree")
+#Write Python code to print I am learning Python
+print("I am learning Python")
+#Write Python code to print Python programming
+print("Python Programming")
+#Write Python code to print Good Morning
+print('Good Morning')
+#Write Python code to print Welcome students
+print("Welcome students")
+#Write Python code to print Python is easy
+a="Python is easy"
+print(a)
+#Write Python code to print My First Program
+x='My First Program'
+print(x)
+#Write Python code to print Hello from Python 
+print("Hello from Python")
